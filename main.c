cb86ee23f8a64cbc7982f4a8260dea3c39423d4b
AniKrisn <anirudh.krishnan@hotmail.com>
2024-10-17 00:01:47 +0530

    object, type

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 
+typedef enum {
+    OBJ_INT,
+    OBJ_PAIR
+} ObjectType;
+
+typedef struct sObject {
+    ObjectType type;
+
+    union {
+        /* OBJ_INT */
+        int value;
+
+        /* OBJ_PAIR */
+        struct {
+            struct sObject* head;
+            struct sObject* tail;
+        };
+    };
+} Object;
 
 
